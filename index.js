69e4c816e708f0c3dd0999db660a2e780233e15e
Alena Trushnikova <alena.yu.t@gmail.com>
2021-01-12 11:00:33 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// no longer has DOM node 'main#main'
+main.remove()
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+// with "YOUR-NAME is the champion" inside
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+newHeader.innerHTML = 'YOUR-NAME is the champion'
